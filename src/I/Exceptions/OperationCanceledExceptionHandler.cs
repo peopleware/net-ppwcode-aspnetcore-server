76ead89d38dc29342a6e6c8c4ab7e88684cca5f8
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2026-02-13 22:42:05 +0100

    OperationCanceledExceptionHandler: fix typo

--- src/I/Exceptions/OperationCanceledExceptionHandler.cs
+++ src/I/Exceptions/OperationCanceledExceptionHandler.cs
@@ -7,10 +7,10 @@ using Microsoft.Extensions.Hosting;
 namespace PPWCode.AspNetCore.Server.I.Exceptions;
 
 [ExcludeFromCodeCoverage]
-public sealed class OperationCancelledExceptionHandler
-    : BaseExceptionHandler<OperationCancelledExceptionHandler, OperationCanceledException>
+public sealed class OperationCanceledExceptionHandler
+    : BaseExceptionHandler<OperationCanceledExceptionHandler, OperationCanceledException>
 {
-    public OperationCancelledExceptionHandler(ProblemDetailsFactory problemDetailsFactory, IHostEnvironment environment)
+    public OperationCanceledExceptionHandler(ProblemDetailsFactory problemDetailsFactory, IHostEnvironment environment)
         : base(problemDetailsFactory, environment)
     {
     }
